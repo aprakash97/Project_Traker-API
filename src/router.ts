832c6f6ad31e57de0a86047d0b5c938bc37ae71c
Prakash <prakashakrakr@gmail.com>
2023-03-14 21:57:48 +0530

    router routes

---
+++ src/router.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+const router = Router(); //Router function
+
+//Product routes
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product", () => {});
+router.delete("/product/:id", () => {});
+
+//Update routes
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.post("/update", () => {});
+router.delete("/update/:id", () => {});
+
+//UpdatePoint routes
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put("/updatepoint/:id", () => {});
+router.post("/updatepoint", () => {});
+router.delete("/updatepoint/:id", () => {});
