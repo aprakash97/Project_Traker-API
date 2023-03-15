579d32db9b70e61d1c55556f664928285a8e65fd
Prakash <prakashakrakr@gmail.com>
2023-03-15 23:52:57 +0530

    authentication server breaking

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -10,12 +10,31 @@ export const createJWT = (user) => {
   //authorization header
 };
 
-export const protect = (req, res) => {
+export const protect = (req, res, next) => {
   const bearer = req.headers.authorization;
 
   if (!bearer) {
     res.status(401);
     res.json({ message: "not authorized" });
+    return;
+  }
+
+  const [, token] = bearer.split(" ");
+
+  if (!token) {
+    res.status(401);
+    res.json({ message: "not valid token" });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+    res.status(401);
+    res.json({ message: "not valid token" });
     return;
   }
 };
