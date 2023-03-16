e3903a0c65d9b861118a97d22ca5cd0ca61da24c
Prakash <prakashakrakr@gmail.com>
2023-03-16 18:10:28 +0530

    signup signin routes

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import router from "./router";
 import morgan from "morgan";
 import { protect } from "./modules/auth";
+import { createNewUser, signin } from "./handlers/user";
 
 const app = express();
 
@@ -25,5 +26,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", protect, router);
+
+app.post("/user", createNewUser);
+app.post("/signin", signin);
 
 export default app;
