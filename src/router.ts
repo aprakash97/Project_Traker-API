81f2b6a967efbf1edfb6bf6aa98e0bb1b615970e
Prakash <prakashakrakr@gmail.com>
2023-03-17 12:25:30 +0530

    handleinputerrors for update - middleware

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middlewares";
 
 const router = Router(); //Router function
+
+//input validator-express validator
 
 //Product routes
 router.get("/product", (req, res) => {
@@ -9,7 +13,12 @@ router.get("/product", (req, res) => {
   res.json({ message: req.shhhh_secret });
 });
 router.get("/product/:id", () => {});
-router.put("/product/:id", () => {});
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
 router.post("/product", () => {});
 router.delete("/product/:id", () => {});
 
