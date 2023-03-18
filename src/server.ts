08767dbf8f891d8aa5cae4440c918ca819ec66b1
Prakash <prakashakrakr@gmail.com>
2023-03-18 15:16:50 +0530

    error handling

--- src/server.ts
+++ src/server.ts
@@ -13,21 +13,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //custom middleware
-app.use((req, res, next) => {
-  req.shhhh_secret = "doggy";
-  next();
-});
+// app.use((req, res, next) => {
+//   req.shhhh_secret = "doggy";
+//   next();
+// });
+
 //executed when page viewed on browser on URL
+
 //Registering listeners
-app.get("/", (req, res) => {
-  console.log("hello form express");
-  res.status(200);
-  res.json({ message: "hello" });
+// app.get("/", (req, res) => {
+//   console.log("hello form express");
+//   res.status(200);
+//   res.json({ message: "hello" });
+// });
+
+//ERROR HANDLER
+app.get("/", (req, res, next) => {
+  setTimeout(() => {
+    next(new Error("sfaafsafsd"));
+  }, 1);
 });
 
 app.use("/api", protect, router);
-
 app.post("/user", createNewUser);
 app.post("/signin", signin);
+
+//ERROR HANDLER
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.json({ message: `Oops Error ${err.message}` });
+});
 
 export default app;
